109c4762ad01d44b5ecac14e5261acd1724c33af
Harish S <toharishs@gmail.com>
2026-07-21 11:08:06 +0530

    ims_ipsec_pcscf: add 3GPP Rel-18 IPsec enhancements

--- src/modules/ims_ipsec_pcscf/run_spi_list_tests.sh
+++ src/modules/ims_ipsec_pcscf/run_spi_list_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-gcc -D_IPSEC_SPI_LIST_TEST spi_list_tests.c spi_list.c -o spi_list_tests
+# build tests
+gcc -D_IPSEC_SPI_LIST_TEST -I../../ -o spi_list_tests spi_list.c spi_list_tests.c ipsec_alg.c sec_agree.c cmd.c ipsec.c ipsec_evt.c -lmnl
+# run tests
 ./spi_list_tests
